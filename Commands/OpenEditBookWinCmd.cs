f5fc3d262cbbe326af841f1c4406ce5ca360a71f
Николай Попов <casper464@yandex.ru>
2023-05-19 12:18:26 +0700

    Добавлено окно для редактирования данных о книге, ViewModel окна, команда открытия окна, привязка команды к пункту меню.

---
+++ Commands/OpenEditBookWinCmd.cs
@@ -0,0 +1,29 @@
+﻿using Cataloguer.Commands.Base_command;
+using Cataloguer.Views;
+using System;
+using System.Windows;
+
+namespace Cataloguer.Commands
+{
+    internal class OpenEditBookWinCmd : BaseCommand
+    {
+        private EditBookWindow _Window;
+        public override bool CanExecute(object parameter) => _Window == null;
+        public override void Execute(object parameter)
+        {
+            EditBookWindow window = new EditBookWindow
+            {
+                Owner = Application.Current.MainWindow
+            };
+            _Window = window;
+            window.Closed += OnWindowClosed;
+            window.ShowDialog();
+        }
+
+        private void OnWindowClosed(object Sender, EventArgs E)
+        {
+            ((Window)Sender).Closed -= OnWindowClosed;
+            _Window = null;
+        }
+    }
+}
---
+++ ViewModels/EditBook_VM.cs
@@ -0,0 +1,111 @@
+﻿using Cataloguer.Models;
+using Cataloguer.ViewModels.ValidatableViewModelBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cataloguer.ViewModels
+{
+    internal class EditBook_VM : ValidatableVMBase
+    {
+
+        #region заголовок окна
+        private string title = "Редактирование данных о издании";
+        /// <summary>Заголовок окна </summary>
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+        }
+        #endregion
+
+
+        #region Свойства для передачи строковых данных из представления       
+
+        /// <summary>Название произведения</summary>
+        private string titleBook;
+        public string TitleBook
+        {
+            get => titleBook;
+            set => Set(ref titleBook, value);
+        }
+
+        /// <summary>Жанры произведений</summary>
+        private int selectedGenres;
+        public int SelectedGenres
+        {
+            get => selectedGenres;
+            set => Set(ref selectedGenres, value);
+        }
+
+        /// <summary>Год издания</summary>
+        private string yearPublicationBook;
+        public string YearPublicationBook
+        {
+            get => yearPublicationBook;
+            set => Set(ref yearPublicationBook, value);
+        }
+        /// <summary>Краткое описание произведения</summary>
+        private string contentBook;
+        public string ContentBook
+        {
+            get => contentBook;
+            set => Set(ref contentBook, value);
+        }
+
+
+        #endregion
+
+
+
+
+        public EditBook_VM()
+        {
+
+        }
+
+
+        /// <summary>переопределенный метод проверки на валидность введенной пользователем информации.</summary>
+        protected override string Validate(string columnName)
+        {
+            if (columnName == nameof(YearPublicationBook))
+            {
+                if (String.IsNullOrWhiteSpace(YearPublicationBook))
+                {
+                    return "Поле 'Год издания' не может быть пустым";
+                }
+                if (!Int32.TryParse(YearPublicationBook, out var value))
+                {
+                    return "В данное поле допускается ввод только цифр! (целое число)";
+                }
+            }
+
+            return String.Empty;
+        }
+
+        ///<summary>Метод на корректный ввод данных о годе издания книги</summary>
+        private bool ValidateEnterYearPublicationBook(Book _book, string yearPublicationBook)
+        {
+            if (!Regex.IsMatch(yearPublicationBook, @"^[0-9]+$") || yearPublicationBook is null)
+            {
+                MessageBox.Show("Игнорируя предупреждения вы все таки допустили ошибку!", "Внимание! Исправьте ввод года издания!",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            if (Convert.ToInt32(yearPublicationBook) <= 0)
+            {
+                MessageBox.Show("Игнорируя предупреждения вы все таки допустили ошибку!", "Внимание! Число может быть только положительным и больше 0!",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            _book.YearPublication = Convert.ToUInt32(yearPublicationBook);
+            return true;
+
+        }
+    }
+}
