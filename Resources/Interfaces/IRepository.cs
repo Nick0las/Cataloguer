e15b4328072ad0a406d20c793835421967c6c27a
Николай Попов <casper464@yandex.ru>
2023-05-20 08:56:11 +0700

    Репозиторий

---
+++ Resources/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Cataloguer.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cataloguer.Resources.Interfaces
+{
+    public interface IRepository <T> where T : IEntity
+    {
+        void Add(T item);
+
+        IEnumerable<T> GetAll();
+
+        T Get(int id) => GetAll().FirstOrDefault(item => item.Id == id);
+
+        void Remove(T item);
+
+        void Update(int id, T item);
+    }
+}
